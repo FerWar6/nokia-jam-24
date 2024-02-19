1c6ec38377af4ae83141e3dc9b8c539712debb94
gingerappa <t.swinkels@sintlucasedu.nl>
2024-02-19 21:07:38 +0100

    trying to get flued

--- Assets/Scripts/fuelMovement.cs
+++ Assets/Scripts/fuelMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class fuelMovement : MonoBehaviour
 {
     public int fuelAmount;
+    public float movementSpeed;
 
     // Update is called once per frame
     void Update()
     {
         float input = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(input, 4, 0)
+        Vector3 movement = new Vector3(input, 0, 0) * movementSpeed * Time.deltaTime;
+
+        transform.Translate(movement);
     }
 }
